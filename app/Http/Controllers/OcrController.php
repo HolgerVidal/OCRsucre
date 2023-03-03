f0d7596da1de031ac38da191667ebd31553ef500
HolgerVidal <holgervidal91@gmail.com>
2023-03-02 22:44:01 -0500

    update

--- app/Http/Controllers/OcrController.php
+++ app/Http/Controllers/OcrController.php
@@ -193,4 +193,58 @@ class OcrController extends Controller
 
     }
 
+    public function obtener_texto_img(Request $request){
+
+		try{
+			
+            set_time_limit(0);
+            ini_set('memory_limit','-1');
+
+            //obigen del documentos
+            //TRAMITES = el cliente tiene que guardar el documento en 'Docs' y solo recivimos el nombre | finalmente nosotros lo borramos
+            //EXTERNO = el cliente soli envia el nombre, nosotros tenemos que sacar el documento del servidor de archivos
+
+            $nombre_imagen = $request->nombre_imagen;
+			$tiempo_inicial = microtime(true);
+    
+            $extension = pathinfo($nombre_imagen, PATHINFO_EXTENSION); //solo la extension del documento
+			$tmpFile = pathinfo($nombre_imagen, PATHINFO_FILENAME); //solo el nombre del documento
+            $ruta = base_path("docs");
+
+            //borramos el archivo temporal
+            // Storage::disk("archivosLocal")->delete($tmpFile.".".$extension);
+
+            $text_res = "";
+  
+            $cmd = "tesseract $ruta/$tmpFile.$extension $ruta/$tmpFile";
+            exec($cmd, $salida, $err);
+            $res = file_get_contents("$ruta/$tmpFile.txt");
+            @unlink("$ruta/$tmpFile.$extension");
+            @unlink("$ruta/$tmpFile.txt");
+            $res=preg_replace("/[\r\n|\n|\r]+/"," ", $res);
+            $text_res = $res; #si no se quiere paginar
+
+            
+            //calculamos el tiempo de ejecucion
+            $tiempo_final = microtime(true);
+            $tiempo = $tiempo_final - $tiempo_inicial;
+            $tiempo = round($tiempo, 2);
+            
+            return response([
+                "error"=>false,
+                "tiempo"=>$tiempo,
+                "tiempounidad"=>"segundos",
+                "numpaginas"=>$num_paginas,
+                "response"=>$text_res
+            ]);
+          
+        }catch(\Throwable $th){
+            // header("Content-Type: application/json; charset=UTF-8");
+            $msg_error = __CLASS__." => ".__FUNCTION__." => Mensaje => ".$th->getMessage()." => en la linea: ".$th->getLine();
+            Log::error($msg_error);
+            dd($msg_error);
+        }
+
+    }
+
 }
